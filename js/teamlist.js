74a33e44f2b7d244fe743dc0a1f5c30cc3195aa5
Daniel Silva <8danielsilva.s@gmail.com>
2023-11-20 00:48:30 -0300

    teamlist.js added

---
+++ js/teamlist.js
@@ -0,0 +1,12 @@
+let teamList = {
+    Barcelona: {
+        id: '001',
+        country: 'Spain',
+        name: 'Barcelona',
+        nationalLeagueTitles: 27,
+        nationalCupTitles: 31,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 5,
+        clubWorldCupTitles: 3
+    }
+}
