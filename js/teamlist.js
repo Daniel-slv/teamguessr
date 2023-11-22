a7d15d4246b218559854f0c7cf5f68020db18c2c
Daniel Silva <8danielsilva.s@gmail.com>
2023-11-22 11:17:42 -0300

    added teams

--- js/teamlist.js
+++ js/teamlist.js
@@ -1,6 +1,7 @@
 let teamList = {
     Barcelona: {
         id: '001',
+        region: 'UEFA',
         country: 'Spain',
         name: 'Barcelona',
         nationalLeagueTitles: 27,
@@ -8,5 +9,16 @@ let teamList = {
         leagueCupTitles: 0,
         uclOrLibertadoresTitles: 5,
         clubWorldCupTitles: 3
+    },
+    RealMadrid: {
+        id: '002',
+        region: 'UEFA',
+        country: 'Spain',
+        name: 'Real Madrid',
+        nationalLeagueTitles: 35,
+        nationalCupTitles: 20,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 14,
+        clubWorldCupTitles: 8
     }
 }
