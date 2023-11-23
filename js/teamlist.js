b788dd481fa6976f52c21ef571e8ac857d405a7b
Daniel Silva <8danielsilva.s@gmail.com>
2023-11-23 11:03:31 -0300

    Update teamlist.js

--- js/teamlist.js
+++ js/teamlist.js
@@ -1,7 +1,7 @@
 export const teamList = {
     Barcelona: {
         id: '001',
-        region: 'UEFA',
+        region: 'Europe',
         country: 'Spain',
         name: 'Barcelona',
         nationalLeagueTitles: 27,
@@ -12,7 +12,7 @@ export const teamList = {
     },
     RealMadrid: {
         id: '002',
-        region: 'UEFA',
+        region: 'Europe',
         country: 'Spain',
         name: 'Real Madrid',
         nationalLeagueTitles: 35,
@@ -20,5 +20,83 @@ export const teamList = {
         leagueCupTitles: 0,
         uclOrLibertadoresTitles: 14,
         clubWorldCupTitles: 8
+    },
+    Valencia: {
+        id: '003',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Valencia',
+        nationalLeagueTitles: 6,
+        nationalCupTitles: 8,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    AtléticoMadrid: {
+        id: '004',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Atlético Madrid',
+        nationalLeagueTitles: 11,
+        nationalCupTitles: 10,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    AthleticBilbao: {
+        id: '005',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Athletic Bilbao',
+        nationalLeagueTitles: 8,
+        nationalCupTitles: 23,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    Sevilla: {
+        id: '006',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Sevilla',
+        nationalLeagueTitles: 1,
+        nationalCupTitles: 5,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    RealBetis: {
+        id: '007',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Real Betis',
+        nationalLeagueTitles: 1,
+        nationalCupTitles: 3,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    DeportivoLaCoruña: {
+        id: '008',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Deportivo La Coruña',
+        nationalLeagueTitles: 1,
+        nationalCupTitles: 2,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
+    },
+    RealSociedad: {
+        id: '009',
+        region: 'Europe',
+        country: 'Spain',
+        name: 'Real Sociedad',
+        nationalLeagueTitles: 2,
+        nationalCupTitles: 2,
+        leagueCupTitles: 0,
+        uclOrLibertadoresTitles: 0,
+        clubWorldCupTitles: 0
     }
+
 }
