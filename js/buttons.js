deefbc866f433db0922b4170de5c569a71d9d72c
Daniel Silva <8danielsilva.s@gmail.com>
2023-11-23 11:03:26 -0300

    Create buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,25 @@
+/* Selecting some HTML elements */
+
+const startGameButton = document.querySelector('#start-game-button');
+const tutorialPopup = document.querySelector('#tutorial-popup');
+const gameContainer = document.querySelector('#game-container');
+const continueButton = document.querySelector('#continue-button');
+const scoreText = document.querySelector('#score-container');
+const answerInput = document.querySelector('#answer-input');
+
+/* Buttons behavior */
+
+startGameButton.onclick = () => {
+    tutorialPopup.classList.add('active');
+    gameContainer.classList.add('active');
+}
+
+continueButton.onclick = () => {
+    tutorialPopup.classList.remove('active');
+    gameContainer.classList.remove('active');
+    startGameButton.classList.add('ongoing-game');
+    scoreText.classList.add('ongoing-game');
+    answerInput.classList.add('ongoing-game');
+}
+
+
