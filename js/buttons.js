f8d9fa6e22e82b979e5a407da81cb422dc786b21
Daniel Silva <8danielsilva.s@gmail.com>
2023-11-28 16:27:48 -0300

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -1,27 +1,9 @@
 /* Selecting some HTML elements */
 
 export const startGameButton = document.querySelector('#start-game-button');
-const tutorialPopup = document.querySelector('#tutorial-popup');
-const gameContainer = document.querySelector('#game-container');
-const continueButton = document.querySelector('#continue-button');
-const scoreText = document.querySelector('#score-container');
-const answerInput = document.querySelector('#answer-input');
-const titleList = document.querySelector('#title-list');
-
-/* Buttons behavior */
-
-startGameButton.onclick = () => {
-    tutorialPopup.classList.add('active');
-    gameContainer.classList.add('active');
-}
-
-continueButton.onclick = () => {
-    tutorialPopup.classList.remove('active');
-    gameContainer.classList.remove('active');
-    startGameButton.classList.add('ongoing-game');
-    scoreText.classList.add('ongoing-game');
-    answerInput.classList.add('ongoing-game');
-    titleList.classList.add('ongoing-game')
-}
-
-
+export const tutorialPopup = document.querySelector('#tutorial-popup');
+export const gameContainer = document.querySelector('#game-container');
+export const continueButton = document.querySelector('#continue-button');
+export const scoreText = document.querySelector('#score-container');
+export const answerInput = document.querySelector('#answer-input');
+export const titleList = document.querySelector('#title-list');
