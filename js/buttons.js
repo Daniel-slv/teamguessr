daaf3b7ad80d6faae800b0ba8ad170b21890ba84
Daniel Silva <8danielsilva.s@gmail.com>
2023-12-01 12:22:46 -0300

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -7,3 +7,4 @@ export const continueButton = document.querySelector('#continue-button');
 export const scoreText = document.querySelector('#score-container');
 export const answerInput = document.querySelector('#answer-input');
 export const titleList = document.querySelector('#title-list');
+export const inputsContainer = document.querySelector('#inputs-container')
