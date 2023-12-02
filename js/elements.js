9559fcf30244c158f4008abad103823b5c236f28
Daniel Silva <8danielsilva.s@gmail.com>
2023-12-02 00:07:36 -0300

    Create elements.js

---
+++ js/elements.js
@@ -0,0 +1,22 @@
+/* SELECTING THE HTML ELEMENTS */
+
+// Header elements:
+
+export const navigationMenu = document.querySelector('#navigation-menu');
+export const hamburgerButton = document.querySelector('#hamburger-button');
+export const settingsButton = document.querySelector('#settings-button');
+
+// Game elements:
+
+export const gameContainer = document.querySelector('#game-container');
+export const startGameButton = document.querySelector('#start-game-button');
+export const tutorialPopup = document.querySelector('#tutorial-popup');
+export const continueButton = document.querySelector('#continue-button');
+export const titleList = document.querySelector('#title-list');
+export const inputsContainer = document.querySelector('#inputs-container')
+export const answerInput = document.querySelector('#answer-input');
+export const checkButton = document.querySelector("#check-button");
+
+
+
+
